121cb32f862ca7fcac0bf28ec2fa059882af254a
Rayza Lima <62263757+RayzaLima@users.noreply.github.com>
2022-05-23 19:47:07 -0300

    implements artigoController

--- src/main/java/com/nassau/blog/controller/ArtigoController.java
+++ src/main/java/com/nassau/blog/controller/ArtigoController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ArtigoController {
     
     @PostMapping(path = "/artigo")
-    public @ResponseBody Artigo adicionarArtigo(@RequestBody ArtigoDTO artigoDTO) {
+    public @ResponseBody Artigo adicionarArtigos(@RequestBody ArtigoDTO artigoDTO) {
         Artigo artigo = new Artigo();
 
         artigo.setTitulo(artigoDTO.getTitulo());
@@ -24,6 +24,40 @@ public class ArtigoController {
         artigo.setTexto(artigoDTO.getTexto());
 
         return artigoLocalService.criarArtigo(artigo);
+    }
+    
+    @GetMapping(path = "/artigo")
+    public @RequestBody Page<Artigo> buscarArtigos (
+           @RequestParam(valeu = "page", defaultValue = "0") String page,
+           @requestParam(value = "size", defaultValue = "0") String size) {
+    
+        Pageable paging = PageRequest.of(Integer.parseInt(page), Integer.parseInt(size));
+        
+        return artigoLocalService.buscarArtigos(paging);
+    }
+    
+    @GetMapping(path = "/artigo/{id}")
+    public @ResponseBody Object buscarArtigoPeloId(@PathVariable("id) String id) {
+    
+        try {
+            return artigoLocalService.buscarArtigoPeloId(Integer.parseInt(id))
+            } cath (ArtigoInexistenteException e) {
+                return e.getMensage()
+            }
+    }
+    
+    @PutMapping(path = "/artigo/{id}")
+    public @ResponseBody String atualizarArtigo(
+            @PathVariable("id") String id, @RequestBody ArtigoDTO artigoDTO) {
+        try {
+            artigoLocalService.atualizarArtigo(Integer.parseInt(id), 
+            artigoDTO.getTitulo(), artigoDTO.getAutor(),
+            ArtigoHelper.converterStringParaDate(artigoDTO.getData()), artigoDTO.getTexto());
+            
+            return "Artigo Atualizado.";
+       } cath (ArtigoInexistenteException e) {
+                return e.getMensage()
+       }
     }
 
     @Autowired
